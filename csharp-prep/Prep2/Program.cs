5107086e65d6811c415b190aae45b4d6977c7174
Lucas Mendonça <lms.devop@gmail.com>
2024-01-10 08:58:05 -0300

    Prep1 and Prep2 done

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,67 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your score?");
+        string number = Console.ReadLine();
+        int courseGrade = int.Parse(number);
+
+        string letter;
+
+        if (courseGrade >= 90)
+        {
+            letter = "A";
+        }
+        else if (courseGrade >= 80)
+        {
+            letter = "B";
+        }
+        else if (courseGrade >= 70)
+        {
+            letter = "C";
+        }
+        else if (courseGrade >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        int lastDigit = courseGrade % 10;
+        string sign = "";
+
+        if (letter != "F")
+        {
+            if (lastDigit >= 7)
+            {
+                sign = "+";
+            }
+            else if (lastDigit < 3)
+            {
+                sign = "-";
+            }
+        }
+
+        if (letter == "A" && lastDigit >= 7)
+        {
+            letter = "A-";
+            sign = "";
+        }
+        else if (letter == "F")
+        {
+            sign = "";
+        }
+
+        Console.WriteLine($"Your grade is: {letter}{sign}");
+
+        if (courseGrade >= 70)
+        {
+            Console.WriteLine("Congratulations! You passed the course.");
+        }
+        else
+        {
+            Console.WriteLine("Don't worry, keep working hard for next time!");
+        }
     }
 }
