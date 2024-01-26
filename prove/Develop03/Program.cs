c5e4aa69148d455d754eb9df1431856871213fdd
Lucas Mendonça <lms.devop@gmail.com>
2024-01-26 11:40:06 -0300

    Finish Assignment w03

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -4,6 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop03 World!");
+        string scriptureText = "Trust in the Lord with all your heart and lean not on your own understanding; in all your ways submit to him, and he will make your paths straight.";
+        Reference reference = new Reference("Proverbs", 3, 5, 6);
+
+
+        string[] wordsArray = scriptureText.Split(' ');
+        List<Word> wordsList = new List<Word>();
+        foreach (string wordText in wordsArray)
+        {
+            wordsList.Add(new Word(wordText));
+        }
+
+        Scripture scripture = new Scripture(reference, wordsList);
+
+        do
+        {
+            Console.Clear();
+            scripture.GetDisplayText();
+
+            Console.WriteLine("\nPress Enter to continue or type 'quit' to finish:");
+            string input = Console.ReadLine();
+
+            if (input.ToLower() == "quit")
+                break;
+
+            scripture.HideRandomWords(1);
+
+        } while (!scripture.IsCompletelyHidden());
+
+
     }
 }
